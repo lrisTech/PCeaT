f8bad44ec4287e5e4aadbb184f02a05986659605
Kaela Gobencion <kaelaag@bu.edu>
2019-10-17 23:23:06 -0400

    Created backend files

---
+++ backend/controllers/pceatController.js
@@ -0,0 +1,9 @@
+exports.processTime = function(req, res) {
+  // Store data in variable
+  var time = req.params.time;
+  var boothNum = req.params.boothNum;
+  // Add the seat and time to the database
+
+  // Check if there are no errors
+  // If no errors, return confirmation
+};
