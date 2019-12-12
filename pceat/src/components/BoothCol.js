bdb72d63c3ddc2ce8825a5b581a9e69063b3bf34
Joseph Lai <josephl@bu.edu>
2019-12-12 18:40:24 -0500

    Added changes

--- pceat/src/components/BoothCol.js
+++ pceat/src/components/BoothCol.js
@@ -6,11 +6,18 @@ import Col from 'react-bootstrap/Col';
 class BoothCol extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            checkedIn: false,
+            opacity: 1
+        }
     }
+
+    
+
     render(){
         if(this.props.booth){
             return(
-                <Col style={{backgroundColor:"#fffff", paddingLeft: "0px", paddingRight: "0px",}}><button style ={{backgroundColor:this.props.backgroundColor, height:"100%",width:"100%", borderRadius:"10px"}} onClick={()=>this.props.onClick(this.props.id)}/></Col>
+                <Col id="test" style={{opacity: this.state.opacity, backgroundColor:"#fffff", paddingLeft: "0px", paddingRight: "0px",}}><button style ={{backgroundColor:this.props.backgroundColor, height:"100%",width:"100%", borderRadius:"10px"}} onClick={()=>this.setState({opacity: 0.7})}/></Col>
             )
         }else{
             return(
